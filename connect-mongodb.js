6e02f78497909c14ded50778db10c42ba76826a8
Akramsarker <dev.mdakram@gmail.com>
2022-10-09 15:40:53 +0600

    Add env variable

--- connect-mongodb.js
+++ connect-mongodb.js
@@ -1,15 +1,11 @@
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-// if (process.env.NODE_ENV === "local") {
-//   dotenv.config();
-// }
+if (process.env.NODE_ENV === "local") {
+  dotenv.config();
+}
 
-// const url = process.env.MONGO_DB_URI;
-const url =
-  "mongodb+srv://test:kk2WjAScUlSUHdOM@cluster0.y55ewj3.mongodb.net/?retryWrites=true&w=majority";
-
+const url = process.env.MONGO_DB_URI;
 const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
